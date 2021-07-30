116e20879a4e6fddaa4673aff28f82d198bbf3ae
Hyperfire RS <76081146+HyperfireRS@users.noreply.github.com>
2021-07-29 18:25:29 -0600

    Create config.ts

---
+++ src/config.ts
@@ -0,0 +1,39 @@
+export default {
+  bot: {
+    token: "ODYyNzAxOTYzODQ1MTczMjU4.YOcL0A.6h0GduAH3x9SiBp3vTiZDr_SwSs",
+    prefix: ">>",
+    owners: ["798675881819111424"],
+    axis: "100690330336129024",
+  },
+  db: {
+    mongoURI: "mongodb+srv://mcutils:mcutils@hypers-utilities.dqsni.mongodb.net/",
+  },
+  channels: {
+    modLogChannel: "799447102794367012",
+    logChannel: "863505832846557194",
+    botCmdChannel: "848550393264865290",
+  },
+  mainGuildId: "799443569848745984",
+  roles: {
+    muteRole: "800203816376860712",
+    managerRoles: [
+      "799445133368098858",
+    ],
+  },
+  rules: [
+    "",
+    "Do not use any racial slurs, or be racist in any way.",
+    "No voice changers in VC, and do not earrape.",
+    "Do not impersonate anybody famous, or anybody on the server, unless they consent to it.",
+    "Do not spam, in any chat.",
+    "Toxicity is not permitted.",
+    "Keep it SFW, so no Gore or NSFW. (duh)",
+    "You are allowed to swear, but please keep in mind that you can't be toxic.",
+    "No advertising, in any chats or DMs.",
+    "Please speak English only.",
+  ],
+  statuses: [
+    "Get out of my room, I'm playing Minecraft!",
+    "e",
+  ],
+};
